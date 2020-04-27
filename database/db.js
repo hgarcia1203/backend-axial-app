dc54cd2fe955a13d194770d8f611c6de4d99cea9
Hector Garcia <rotceh.1203@gmail.com>
2020-04-26 18:49:10 -0700

    modificacion de db.js para conectar a bd en clever-cloud

--- database/db.js
+++ database/db.js
@@ -1,7 +1,10 @@
 const Sequelize = require('sequelize');
 const db = {}
-const sequelize = new Sequelize("axial", 'root', '', {
-    host: 'localhost',
+
+//cambiar los datos de Sequelize para usar la base de datos local
+//Sequelize(nameDB, usuario, password, {json con datos solo cambiar el host: a localhost})
+const sequelize = new Sequelize("buh4yhzttbvbioekyist", 'uedtpbnpukwgx74t', '0MKG0OlaPVa6Y3WHT6Ss', {
+    host: 'buh4yhzttbvbioekyist-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     operatorsAliases: false,
 
